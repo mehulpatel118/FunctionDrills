2e787565cb0b048c5283ec96fe509f24a8f6973b
Mehul Patel <mehulpatel118@gmail.com>
2019-02-19 14:38:25 -0500

    commit all of function drills 1

--- function.js
+++ function.js
@@ -1,13 +1,45 @@
-
+'use strict';
 function getYearOfBirth(age)
 {
-    return 2019-age;
-}
-function createGreeting(name, age){
-const yearOfBirth = 2019 - age;
-return `Hi, my name is ${name} and I\'m ${age} years old. I was born in ${yearOfBirth}`;
+  if (age < 0)
+  {
+    throw new Error ('Age can not be negative')
+  }
+  return 2019-age;
 }
 
-const greeting1 = createGreeting('Chris', 30);
-console.log(greeting1);
-console.log(getYearOfBirth(30));
+function createGreeting(name, age){
+  if(name === undefined)
+  {
+    throw new Error ('name must be defined');
+  }
+  if(age === undefined)
+  {
+    throw new Error ('age must be defined');
+  }
+  console.log(typeof(age));
+  if(typeof(name)!== 'string')
+  {
+    throw new TypeError('name is not a string');
+  }
+
+  if(typeof(age)!== 'number')
+  {
+    throw new TypeError('Age is not a number');
+  }
+
+  let yob = getYearOfBirth(age);
+  return `Hi, my name is ${name} and I\'m ${age} years old. I was born in ${yob}`;
+}
+
+
+
+try {
+  const greeting1 = createGreeting('bill','bill');
+  console.log(greeting1);
+
+}
+catch (e)
+{
+  console.log(e.message);
+}
