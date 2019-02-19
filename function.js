f9c55e1179296f91c524af356bc8ea06270de25e
Mehul Patel <mehulpatel118@gmail.com>
2019-02-19 13:27:11 -0500

    added year of birth

--- function.js
+++ function.js
@@ -1,8 +1,8 @@
 
 
 function createGreeting(name, age){
-
-return `Hi, my name is ${name} and I\'m ${age} years old`;
+const yearOfBirth = 2019 - age;
+return `Hi, my name is ${name} and I\'m ${age} years old. I was born in ${yearOfBirth}`;
 }
 
 const greeting1 = createGreeting('Chris', 30);
